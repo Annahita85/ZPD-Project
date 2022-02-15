17e0a94a6179bfe30d7776b0938b6712defd40d4
S.Jafari.K <73299967+Annahita85@users.noreply.github.com>
2022-02-15 14:33:26 +0100

    Add files via upload

--- config.php
+++ config.php
@@ -1,18 +1,34 @@
 <?php
 
-$database = define("DSN" , "mysql:host=10.100.5.220;dbname=ben_test1', 'ben', 'onlinewache");
+// $database = define("DSN" , "mysql:host=10.100.5.220;dbname=ben_test1', 'ben', 'onlinewache");
 
 // try { 
-//     $db = new PDO("mysql:host={$database['localhost']};dbname={$database['tabellename']}", $database['ben'], $database['onlinewache']); 
+//     $db = new mysqli("10.100.5.220", "ben", "onlinewache", "ben_test1", "3306");; 
 // } catch (PDOException $e) { 
 //     die("An error happend, Error: " . $e->getMessage()); 
 // }
 
-try{
-    $db = new pdo('mysql:host=10.100.5.220;port=3306;dbname=tabellename;charset=utf8','ben','onlinewache',array(
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    ));
-    }catch(PDOException $pe){
-        echo $pe->getMessage();
-    }
+// try{
+//     $db = new PDO('mysql:host=10.100.5.220;port=3306;dbname=tabellename;charset=utf8','ben','onlinewache',array(
+//         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+//     ));
+//     }catch(PDOException $e){
+//         echo $e->getMessage();
+//     }
+?>
+
+<?php
+
+define('DB_SERVER', 'host=10.100.5.220');
+define('DB_USERNAME', 'ben');
+define('DB_PASSWORD', 'onlinewache');
+define('DB_NAME', 'tabellenname');
+ 
+
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
 ?>
