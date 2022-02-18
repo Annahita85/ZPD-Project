f93bceadc0ac3622ff948385a295f0c007ec2d71
S.Jafari.K <73299967+Annahita85@users.noreply.github.com>
2022-02-18 14:31:09 +0100

    Add files via upload

--- einfach php/login.php
+++ einfach php/login.php
@@ -1,7 +1,7 @@
 <section classs="signup_form">
     <h1>Log IN</h1>
-    <form action ="login.inc.php", method="post">
-        <input type="text" name="name" placeholder="Username/Email...">
+    <form action ="login.inc.php" method="post">
+        <input type="text" name="uid" placeholder="Username/Email...">
         <input type="password" name="pwd" placeholder="Password...">
         <button type="submit" name="submit">Log In</button>
     </form>
