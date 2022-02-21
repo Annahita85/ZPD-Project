b2b3d9d30f064e9d575dac83bedad67ac59f8621
S.Jafari.K <73299967+Annahita85@users.noreply.github.com>
2022-02-21 16:17:09 +0100

    Add files via upload

--- einfach php/reset-password.inc.php
+++ einfach php/reset-password.inc.php
@@ -21,6 +21,7 @@
         $stmt = mysqli_stmst_init($conn);
         if (!mysqli_stmt_prepare($stmt,$sql)) {
             echo "There was an ERROR!";
+            exit();
 
         }else {
             mysqli_stmt_bind_param($stmt , "s" , $selector);
@@ -41,7 +42,52 @@
                     exit();
 
                 } elseif ($tockenCheck === true ) {
-                    
+
+                    $tokenEmail = $row['pwdResetEmail'];
+
+                    $sql = "SELECT * FROM user WHERE emailUsers=?;";
+                    $stmt = mysqli_stmst_init($conn);
+                    if (!mysqli_stmt_prepare($stmt,$sql)) {
+                        echo "There was an ERROR!";
+                        exit();
+                    } else {
+                        mysqli_stmt_bind_param($stmt , "s", $tockenEmail);
+                        mysqli_stmt_execute($stmt);
+                        $result = mysqli_stmst_get_result($stmt);
+                        if (!$row = mysqli_fetch_assoc($result)) {
+                            echo "There was an ERROR!.";
+                            exit();
+                        }else {
+                           
+                            $sql = "UPDATE users SET pwdUsers=? WHERE emailUsers=?";
+                            $stmt = mysqli_stmst_init($conn);
+                            if (!mysqli_stmt_prepare($stmt,$sql)) {
+                                echo "There was an ERROR!";
+                                exit();
+                            } else {
+
+                                $newPwdHash = password_hash($password , PASSWORD_DEFAULT);
+                                mysqli_stmt_bind_param($stmt , "ss", $tockenEmail , $tockenEmail);
+                                mysqli_stmt_execute($stmt);
+
+                                $sql = "DELETE FROM pwdReset WHERE pwdResetEmail=?";
+                                $stmt = mysqli_stmst_init($conn);
+                                if (!mysqli_stmt_prepare($stmt , $sql)) {
+                                    echo "There was an ERROR!";
+                                    exit;
+                                }else {
+                                    mysqli_stmt_bind_param($stmst , "s",$tockenEmail);
+                                    mysqli_stmt_execute($stmt);
+                                    header("Location: signup.php?newpwd=passwordupdated");
+                            
+                                }
+
+                            }
+
+                        }
+
+
+                    }
                 }
             }
         }
