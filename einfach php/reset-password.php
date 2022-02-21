a5205023f9e0727d8dd03d3ac51411f8488b5108
S.Jafari.K <73299967+Annahita85@users.noreply.github.com>
2022-02-21 15:42:16 +0100

    Add files via upload

---
+++ einfach php/reset-password.php
@@ -0,0 +1,15 @@
+<main>
+    <h1> Reset your Password</h1>
+    <p>An email will be send to you with instructions on how to reset your Password.</p>
+    <form action="./reset-password.inc.php" method="post">
+        <input type="text" name="email" placeholder="Enter your Email adresse.....">
+        <button type="submit" name="reset-request-submit">Receive new Password by email</button>
+    </form>
+
+    <?php
+        if (isset($_GET["reset"])) {
+            if ($_GET["reset"] == "success") {
+                echo '<p> Check your Email!</p>';
+            }
+        }
+
