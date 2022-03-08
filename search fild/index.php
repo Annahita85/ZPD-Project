c10e7e12996dbfb8f9c76eabc9157b0fdc29c9ba
S.Jafari.K <73299967+Annahita85@users.noreply.github.com>
2022-03-08 15:11:12 +0100

    Add files via upload

---
+++ search fild/index.php
@@ -0,0 +1,15 @@
+<?php
+include_once 'dbh.php';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+       <p>this is a short text for searching lessen on Computer .</p>
+
+    </body>
+</html>
