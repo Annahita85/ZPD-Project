37d70ffe6f8b9190df7d14bdd25d22d3f00150bb
S.Jafari.K <73299967+Annahita85@users.noreply.github.com>
2022-03-08 10:15:07 +0100

    Add files via upload

---
+++ upload file and image/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <main>
+            <p>SEND E-MAIL</p>
+            <form action="upload.php" method="POST" enctype="multipart/form-data">
+                <input type ="file" name="file" >
+                <button type="submit" name="submit">UPLOAD</button>
+            </form>
+        </main>
+    </body>
+</html>
