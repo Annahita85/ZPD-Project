306b8e9852b5d10d3dbd2c12b8559fe747ae8e31
S.Jafari.K <73299967+Annahita85@users.noreply.github.com>
2022-03-08 08:34:31 +0100

    Add files via upload

---
+++ Contact Form/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Contact Form</title>
+    </head>
+    <body>
+        <main>
+            <p>SEND EMAIL</p>
+            <form action="contactform.php" method="post"></form>
+                <input type ="text" name="name" placeholder="Full name....">
+                <input type ="text" name="mail" placeholder="Your Email....">
+                <input type ="text" name="subject" placeholder="Subject....">
+                <textarea name="massage" placeholder="Message"></textarea>
+                <button type="submit" name="submit">SEND EMAIL</button>
+            </form>
+        </main>
+    </body>
+</html>
