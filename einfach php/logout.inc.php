41d7b9c6470f28a2690dacdb529b0229ce7d63ed
S.Jafari.K <73299967+Annahita85@users.noreply.github.com>
2022-03-09 11:15:52 +0100

    Delete einfach php directory

--- einfach php/logout.inc.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-session_start();
-session_unset();
-session_destroy();
-
-
-header("location:  ../einfach php/index.php");
-exit();
--- einfach php/profile.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if (isset($_SESSION["useruid"])){
-    echo "<p>Hello there ". $_SESSION["useruid"] . "</p>";
-}
-?>
