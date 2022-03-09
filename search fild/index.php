b986e04f8adc97c9519d079cac38dfc29a3ebcec
S.Jafari.K <73299967+Annahita85@users.noreply.github.com>
2022-03-09 11:13:58 +0100

    Add files via upload

--- search fild/index.php
+++ search fild/index.php
@@ -1,15 +1,34 @@
 <?php
-include_once 'dbh.php';
-?>
+    include 'header.php';
+?> 
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title></title>
-    </head>
-    <body>
-       <p>this is a short text for searching lessen on Computer .</p>
+       <form action="search.php" method="POST">
+           <input type="text" name="search" placeholder ="Search...">
+           <button tpye="submit" name ="submit-search" >Search</button>
+       </form>
+
+
+       <h1>Front page</h1>
+       <h2>all article</h2>
+
+       <div class="article-countainer">
+            <?php
+               $sql = "SELECT * FROM dbsearch";
+               $result = mysqli_query($conn, $sql);
+               $queryResults =  mysqli_num_rows($result);
+
+               if ($queryResults > 0) {
+                   while ($row = mysqli_fetch_assoc($result)){
+                        echo "<div class='article-box'>
+                                    <h3>".$row['a_titel']."</h3>
+                                    <p>".$row['a_text']."</p>
+                                    <p>".$row['a_date']."</p>
+                                    <p>".$row['a_author']."</p>
+                                </div>";
+                   }
+                }
+            ?>
+       </div>
 
     </body>
 </html>
